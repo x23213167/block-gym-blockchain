35320dac8ef67460820a92e91936804b53757351
x23216093 <x23216093@student.ncirl.ie>
2024-04-13 22:45:36 +0100

    port changed

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/user", (req, res) => {
 });
 // serving the index.html file
 
-const server = app.listen(process.env.PORT||8080);
+const server = app.listen(process.env.PORT||3000);
 const portNumber = server.address().port;
 console.log(`port: ${portNumber}`);
 // can see the port number in terminal - you can dictate the port number
